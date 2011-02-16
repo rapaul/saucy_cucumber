cdadc1126b7c8d603dfe77c04097406ca3d2edc9
Richard Paul <richard.a.paul@gmail.com>
2011-02-16 22:00:23 +0000

    Sauced it up

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source :rubygems
 
 gem 'cucumber', '0.10.0'
+gem 'sauce'
 gem 'capybara'
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,13 @@
+require 'sauce'
+require 'sauce/capybara'
 require 'capybara/cucumber'
 
 Capybara.run_server = false
 Capybara.app_host = 'http://wikipedia.org'
-Capybara.default_driver = :selenium
+Capybara.default_driver = :sauce
+
+Sauce.config do |config|
+  config.username = '<username>'
+  config.access_key = '<key>'
+end
 
