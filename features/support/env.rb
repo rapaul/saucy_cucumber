27cb7224e7e64f81eb74603fe349f833e1f38f4c
Richard Paul <richard.paul@bbc.co.uk>
2011-02-19 12:05:29 +0000

    Set your sauce username and access key in environment

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,8 +6,3 @@ Capybara.run_server = false
 Capybara.app_host = 'http://wikipedia.org'
 Capybara.default_driver = :sauce
 
-Sauce.config do |config|
-  config.username = '<username>'
-  config.access_key = '<key>'
-end
-
